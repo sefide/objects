eff1a473f5d335d0ace7c616b3dc3e0c4138dc91
sefide <sefide@naver.com>
2025-02-11 00:49:37 +0900

    chore: 데이터 중심 설계 예시 추가

--- src/main/java/com/sefide/objects/move_data_system/Movie.java
+++ src/main/java/com/sefide/objects/move_data_system/Movie.java
@@ -1,12 +1,71 @@
 package com.sefide.objects.move_data_system;
 
+import java.time.Duration;
+import java.util.List;
+
 public class Movie {
     private String title;
-    private Duration ruggningTime;
+    private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
 
     private MovieType movieType; // 영화에 사용된 할인 정책의 종류를 알 수 있는 방법..
     private Money discountAmount;
     private double discountPercent;
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return discountConditions;
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Duration getRuggningTime() {
+        return ruggningTime;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setRuggningTime(Duration ruggningTime) {
+        this.ruggningTime = ruggningTime;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
 }
